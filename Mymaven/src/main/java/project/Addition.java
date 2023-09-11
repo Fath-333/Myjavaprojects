a2e59a7e4f3214ec37c8d676e6d1aa00b3ff7262
Fathima Sharafudeen <Fathima Sharafudeen@DESKTOP-02GK4BS.mshome.net>
2023-09-11 14:49:23 +0530

    Mavenprojects

---
+++ Mymaven/src/main/java/project/Addition.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Addition
+{
+	public static void main(String args[])
+	{
+	int a = 5;
+	int b = 4;
+	int sum = a+b;
+System.out.println(sum);	
+	}
+	}
---
+++ Mymaven/src/main/java/project/Start.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Start {
+	public static void main(String args[])
+	{
+	String s1="Fruit";
+	System.out.println(s1.startsWith("Fr"));
+	System.out.println(s1.startsWith("fr"));
+	System.out.println(s1.startsWith("ar"));
+	}
+	}
+
+
+
